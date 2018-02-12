23eecfc38c843f38a745637db588e42a80e92555
wuxinfu <sz4dd@virginia.edu>
2018-02-12 14:00:31 -0500

    ayyy
    first change

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+class Item(models.Model):
+    item_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class price(models.Model):
+    item = models.ForeignKey(item, on_delete=models.CASCADE)
+    price_text = models.CharField(max_length=200)
+    actualprice = models.IntegerField(default=0)
 # Create your models here.
